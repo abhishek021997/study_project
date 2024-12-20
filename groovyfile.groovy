0c7df298f7d426209137e9f2924d86211d9c008d
abhishek021997 <49532014+abhishek021997@users.noreply.github.com>
2024-12-20 14:02:01 +0530

    Update groovyfile.groovy

--- groovyfile.groovy
+++ groovyfile.groovy
@@ -1,12 +1,19 @@
+@Library("study_shared") _
 pipeline {
     agent { label "dev" }
 
   stages{
-    stage(build){
-      steps{
-        sh 'echo "hi this is testing new"'
+      stage(var_call){
+          steps{
+              
+          }
       }
-    }
+      
+      stage(build){
+          steps{
+            sh 'echo "hi this is testing new"'
+          }
+      }
 
     
   }
