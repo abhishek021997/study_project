d262f44e1f43170eef4cfea5c2b89b6158414c75
abhishek021997 <49532014+abhishek021997@users.noreply.github.com>
2024-12-20 14:47:24 +0530

    sdf

--- groovyfile.groovy
+++ groovyfile.groovy
@@ -1,21 +1,24 @@
-@Library("study_shared") _
+//@Library("study_shared") _
 pipeline {
     agent { label "dev" }
 
+vars{
+    abhi = "12345"
+}
   stages{
-      stage(var_call){
-          steps{
-            variable()
-            
-            
-          }
+    stage(var_call){
+      steps{
+        echo "${vars.abhi}"
+
       }
+    }
       
-      stage(build){
-          steps{
-            sh 'echo "hi this is testing new"'
-          }
-      }
+      
+    stage(build){
+        steps{
+          sh 'echo "hi this is testing new"'
+        }
+    }
 
     
   }
