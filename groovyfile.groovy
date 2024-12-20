af10fe20d590dbd0a5458da45f31d165ccd971ac
abhishek021997 <49532014+abhishek021997@users.noreply.github.com>
2024-12-20 14:38:30 +0530

    dsadc

--- groovyfile.groovy
+++ groovyfile.groovy
@@ -1,16 +1,11 @@
-//@Library("study_shared") _
+@Library("study_shared") _
 pipeline {
     agent { label "dev" }
 
   stages{
       stage(var_call){
           steps{
-            class Example {
-              static void main(String[] args) {
-                  // Using a simple println statement to print output to the console
-                  println('Hello World');
-              }
-            }
+            variable()
             
             
           }
