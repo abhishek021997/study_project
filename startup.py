ec9abe7c73f6cce11932a7061d765820b5a4df2a
abhishek021997 <49532014+abhishek021997@users.noreply.github.com>
2024-12-21 15:17:53 +0530

    sdf

--- startup.py
+++ startup.py
@@ -1,5 +1,5 @@
 import os
-import docker
+import time
 
 """
 Created by: Abhishek Sharma
@@ -8,15 +8,31 @@ data collection and run
 """
 def docker_data(a):
     if a == 0:
-        os.system("docker images")
+        
+        def docker_imge_get(docker_imges):
+            if docker_imges == 0:
+                os.system("docker build -t .")
+                time.sleep(60)
+                os.system("docker-compose run .")
+                
+            elif docker_imges == 1:
+                os.system("docker-compose run .")
+            else:
+                print("Image already pull in your machine")
+
+        docker_imges = os.system("docker images | grep -ic ubuntu/apache2")
+        
+        docker_imge_get(docker_imges)
+
+
     elif a == 1:
         b = os.WEXITSTATUS(os.system(f" {doc_img_output} | grep -i Exited"))
     else:
         print("Container not created")
 
-docker_images = os.WEXITSTATUS(os.system("docker ps -a | grep -i study"))
+docker_process = os.WEXITSTATUS(os.system("docker ps -a | grep -i study"))
 
-doc_img_output = f"{docker_images}"
+doc_img_output = f"{docker_process}"
 
 a = os.WEXITSTATUS(os.system(f" {doc_img_output} | wc -l"))
 
