7386fe7ba548a0b1d84982ed9203e524c3a04f2e
abhishek021997 <49532014+abhishek021997@users.noreply.github.com>
2024-12-21 18:38:35 +0530

    sdf

--- startup.py
+++ startup.py
@@ -8,22 +8,8 @@ data collection and run
 """
 def docker_data(a):
     if a == 0:
+        print(f"{a}")
         
-        def docker_imge_get(docker_imges):
-            if docker_imges == 0:
-                os.system("docker-compose up -d")
-                
-            elif docker_imges == 1:
-                print(f"Image alreay created and runing now{a} {docker_imges}")
-                #os.system("docker-compose up -d")
-
-            else:
-
-                print(f"Image already pull in your machine1{docker_imges}")
-
-        docker_imges = os.WEXITSTATUS(os.system("docker images | grep -ic study"))
-        
-        docker_imge_get(docker_imges)
 
 
     elif a != 0:
