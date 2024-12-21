69a27621b4425154c5c160e63b190ebd6635d437
abhishek021997 <49532014+abhishek021997@users.noreply.github.com>
2024-12-21 16:58:21 +0530

    sdfg

--- startup.py
+++ startup.py
@@ -21,7 +21,7 @@ def docker_data(a):
 
                 print(f"Image already pull in your machine1{docker_imges}")
 
-        docker_imges = os.system("docker images | grep -ic study_img")
+        docker_imges = os.WEXITSTATUS(os.system("docker images | grep -ic study_img"))
         
         docker_imge_get(docker_imges)
 
