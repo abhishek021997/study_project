8fcda9230006226d9fdf11ffb7051c1386540bc3
abhishek021997 <49532014+abhishek021997@users.noreply.github.com>
2024-12-21 18:49:45 +0530

    dsf

--- startup.py
+++ startup.py
@@ -8,17 +8,11 @@ data collection and run
 """
 def docker_data(a):
     if a == 0:
-        def docker_img_make(image_check):
-            if image_check == 0:
-                print(f"this is value {image_check}")
-                
-            else:
-                print(f"this is value else {image_check}")
-                os.system("docker images | grep -ic study")
-
+        
 
         image_check = os.WEXITSTATUS(os.system("docker images | grep -ic study"))
-        docker_img_make(image_check)
+        print(image_check)
+        
 
         
 
