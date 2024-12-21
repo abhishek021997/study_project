bec6969632ff94c71cda770b3fc4a6d21b311666
abhishek021997 <49532014+abhishek021997@users.noreply.github.com>
2024-12-21 18:19:50 +0530

    dsg

--- startup.py
+++ startup.py
@@ -14,7 +14,7 @@ def docker_data(a):
                 os.system("docker-compose up -d")
                 
             elif docker_imges == 1:
-                print(f"Image alreay created and runing now{a}")
+                print(f"Image alreay created and runing now{a} {docker_imges}")
                 #os.system("docker-compose up -d")
 
             else:
