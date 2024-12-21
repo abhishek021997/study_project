6e6d13189bfd9a980b8f4319ecb62c6c017d4954
abhishek021997 <49532014+abhishek021997@users.noreply.github.com>
2024-12-21 15:22:32 +0530

    dsff

--- startup.py
+++ startup.py
@@ -11,7 +11,7 @@ def docker_data(a):
         
         def docker_imge_get(docker_imges):
             if docker_imges == 0:
-                os.system("docker build .")
+                os.system("docker build -t study_img .")
                 time.sleep(60)
                 os.system("docker-composse run .")
                 
