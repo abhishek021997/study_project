99f52b61400cb956bb71dc48c2028597fd11ac53
abhishek021997 <49532014+abhishek021997@users.noreply.github.com>
2024-12-21 14:49:42 +0530

    sdf

--- startup.py
+++ startup.py
@@ -5,20 +5,21 @@ Created by: Abhishek Sharma
 data collection and run
 
 """
-def docker_data(a, docker_images):
+def docker_data(a):
     if a == 0:
-        print(docker_images)
+        docker_all_img = os.WEXITSTATUS(os.system(f"docker ps -a"))
+
+        print(docker_all_img)
     elif a == 1:
-        b = os.system(f" {doc_img_output} | grep -i Exited")
+        b = os.WEXITSTATUS(os.system(f" {doc_img_output} | grep -i Exited"))
     else:
         print("Container not created")
 
-docker_images = os.system("docker ps -a | grep -i study")
+docker_images = os.WEXITSTATUS(os.system("docker ps -a | grep -i study"))
 
 doc_img_output = f"{docker_images}"
 
-a = os.system(f" {doc_img_output} | wc -l")
+a = os.WEXITSTATUS(os.system(f" {doc_img_output} | wc -l"))
 
-docker_data(a, docker_images)
-
+docker_data(a)
 #docker_ps = os.system("docker build .")
